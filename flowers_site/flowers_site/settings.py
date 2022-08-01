5ffa1d5f926e889558ba393f1bad622d6c9c1532
Kate <katyamalysheva2000@yandex.ru>
2022-08-01 14:37:51 +0300

    2struggling with redirecting view

--- flowers_site/flowers_site/settings.py
+++ flowers_site/flowers_site/settings.py
@@ -125,4 +125,4 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-AUTH_USER_MODEL = 'models.СustomUser'
+AUTH_USER_MODEL = 'models.CustomUser'
--- flowers_site/flowers_site/urls.py
+++ flowers_site/flowers_site/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('models/', include('models.urls')),
+    path('', RedirectView.as_view(url='/models/')), 
 ]
+
